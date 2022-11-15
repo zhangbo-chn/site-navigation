14f6c91f61b0737624516d666190e43bd185504e
zhangbo19 <zhangbo19@corp.netease.com>
2022-11-15 23:12:25 +0800

    feat: 16

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/www/' : '/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
